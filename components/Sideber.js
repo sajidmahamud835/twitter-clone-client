2698373e22842226f62800aa17e2db3546c007bb
sajidmahamud835 <sajidmahamud835@gmail.com>
2022-07-04 02:45:41 +0600

    creating sidebar componants

---
+++ components/Sideber.js
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+const Sideber = () => {
+    return (
+        <div>
+            {/* Logo */}
+            <div>
+                <Image width="50" height="50" src="/image/twitterLogo.png"></Image>
+            </div>
+
+            {/* Menu */}
+
+            {/* Button */}
+
+            {/* User Profile */}
+        </div>
+    );
+};
+
+export default Sideber;
