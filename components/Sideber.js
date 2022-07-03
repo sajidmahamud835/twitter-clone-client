039df0768c08a04552a19a4bec04996f2ce8fc9f
sajidmahamud835 <sajidmahamud835@gmail.com>
2022-07-04 04:15:34 +0600

    style updated

--- components/Sideber.js
+++ components/Sideber.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import SidebarMenuItem from "./SidebarMenuItem";
-import { BookmarkIcon, ChatIcon, CogIcon, DotsCircleHorizontalIcon, DotsHorizontalIcon, HashtagIcon, HomeIcon, PhotographIcon, TrendingDownIcon, TrendingUpIcon, UserIcon, ViewListIcon } from "@heroicons/react/solid";
+import { BookmarkIcon, ChatIcon, ClipboardListIcon, CogIcon, DotsCircleHorizontalIcon, DotsHorizontalIcon, HashtagIcon, PhotographIcon, TrendingUpIcon, UserIcon } from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";  // HomeIcon is a component that renders the SVG for the Home icon.
 
 const Sideber = () => {
     const MenuItems = [
@@ -8,7 +9,7 @@ const Sideber = () => {
         { text: "Profile", Icon: UserIcon },
         { text: "Messages", Icon: ChatIcon },
         { text: "Bookmarks", Icon: BookmarkIcon },
-        { text: "Lists", Icon: ViewListIcon },
+        { text: "Lists", Icon: ClipboardListIcon },
         { text: "Moments", Icon: PhotographIcon },
         { text: "Discover", Icon: HashtagIcon },
         { text: "Trends", Icon: TrendingUpIcon },
@@ -30,22 +31,22 @@ const Sideber = () => {
             {/*Tweet Button */}
 
             <div>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-56 h-12 shadow-md text-lg hidden xl:inline">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded-full w-56 h-12 shadow-md text-lg hidden xl:inline">
                     Tweet
                 </button>
             </div>
 
             {/* User Profile */}
-            <div>
-                <div className="flex items-center">
-                    <Image width="50" height="50" src="/image/user.png"></Image>
-                    <div className="ml-2">
-                        <span className="font-bold">User Name</span>
-                        <span className="text-sm">@userName</span>
-                    </div>
+
+            <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
+                <Image width="50" height="50" src="/image/user.png"></Image>
+                <div className="ml-2">
+                    <h4 className="font-bold">User Name</h4>
+                    <p className="text-sm">@userName</p>
                 </div>
+                <DotsHorizontalIcon className="h-5" />
             </div>
-            <DotsHorizontalIcon />
+
         </div>
     );
 };
