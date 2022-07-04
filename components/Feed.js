90b271de5ed1e2da2d1cac82111c2c6dd185cae7
sajidmahamud835 <sajidmahamud835@gmail.com>
2022-07-04 15:10:38 +0600

    tmporary style change

--- components/Feed.js
+++ components/Feed.js
@@ -2,7 +2,7 @@ import { SparklesIcon } from "@heroicons/react/outline";
 
 const Feed = () => {
     return (
-        <div className="xl:ml-[370px] border-r border-l border-gray-200  xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
+        <div className="xl:ml-[370px] border-r border-l border-gray-200  xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl"> 
             <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200 items-center">
                 <h2 className="text-lg sm:text-xl font-bold cursor-pointer">Home</h2>
                 <div className="hoverEffect flex items-center justify-center px-1 ml-auto">
