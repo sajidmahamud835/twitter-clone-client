d1e2d33de7949a96e8a21b3ac4b8f9accab12498
sajidmahamud835 <sajidmahamud835@gmail.com>
2022-07-04 13:06:09 +0600

    updating basic style, creating custom css, adding useState for active, adding commnets for developers, students and employeers as this project is  going to my portfolio

--- components/SidebarMenuItem.js
+++ components/SidebarMenuItem.js
@@ -1,8 +1,25 @@
 const SidebarMenuItem = ({ text, Icon, active }) => {
+    // The SidebarMenuItem component takes a text and icon property.
+    // Used in the Sideber component. Path: twitter-clone-client\components\Sideber.js
     return (
         <div className="hoverEffect flex items-center text-gray-700 justify-center xl:justify-start text-lg space-x-3">
-            <Icon className="h-7" />
+            {/* Note: 
+                // The hoverEffect class is used to add a hover effect to the menu item.
+                //space-x-3 is used to space the menu items out. 
+                // The text-lg property is used to set the font size of the menu items.
+                // The flex items-center property is used to align the menu items vertically.
+                // The justify-center property is used to align the menu items horizontally.
+                // The xl:justify-start property is used to align the menu items horizontally on the large screen. 
+            */}
+            <Icon className="h-7" /> {/* Icon is a component that renders the SVG for the menu item. */}
             <span className={`${active && "font-bold"} hidden xl:inline`}>{text}</span>
+            {/* Note:
+                // text is a string that contains the text for the menu item.
+                // The active property is a function that takes a string and returns a boolean.
+                // The active property is used to determine whether the menu item is active or not.
+                // The ${active && "font-bold"} property is used to add a font weight to the menu item if it is active.
+                // The hidden xl:inline property is used to hide the menu item on the large screen. 
+            */}
         </div>
     );
 };
--- components/Sideber.js
+++ components/Sideber.js
@@ -1,11 +1,13 @@
-import Image from "next/image";
+import Image from "next/image";  // Image is a component that renders an image in nextjs.
 import SidebarMenuItem from "./SidebarMenuItem";
 import { BookmarkIcon, ChatIcon, ClipboardListIcon, CogIcon, DotsCircleHorizontalIcon, DotsHorizontalIcon, HashtagIcon, PhotographIcon, TrendingUpIcon, UserIcon } from "@heroicons/react/outline";
 import { HomeIcon } from "@heroicons/react/solid";  // HomeIcon is a component that renders the SVG for the Home icon.
+import { useState } from "react"; // useState is a hook that is used to create a state variable.
 
 const Sideber = () => {
+    const [active, setActive] = useState(false);
     const MenuItems = [
-        { text: "Home", Icon: HomeIcon, active: active => active === "home" },
+        { text: "Home", Icon: HomeIcon, active: active => active === "home" }, // The active property is a function that takes a string and returns a boolean. The active property is used to determine whether the menu item is active or not.
         { text: "Profile", Icon: UserIcon },
         { text: "Messages", Icon: ChatIcon },
         { text: "Bookmarks", Icon: BookmarkIcon },
@@ -16,6 +18,8 @@ const Sideber = () => {
         { text: "Settings", Icon: CogIcon },
         { text: "More", Icon: DotsCircleHorizontalIcon }
     ];
+    // MenuItems is an array of objects that contain the text and icon for each menu item.
+
     return (
         <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full">
             {/* Logo */}
@@ -26,12 +30,17 @@ const Sideber = () => {
             {/* Menu */}
             <div className="mt-4 mb-2.5 xl:items-start">
                 {
-                    MenuItems.map(item => <SidebarMenuItem text={item.text} Icon={item.Icon} active={item.active} />)
+                    // Loop through each menu item and render a SidebarMenuItem component for each item.
+                    // The SidebarMenuItem component takes a text and icon property.
+                    // The text property is a string that contains the text for the menu item.
+                    // The icon property is a component that renders the SVG for the menu item. 
+                    // The active property is a function that takes a string and returns a boolean.
+                    // The active property is used to determine whether the menu item is active or not.
 
+                    MenuItems.map(item => <SidebarMenuItem key={item.text} text={item.text} Icon={item.Icon} active={item.active} />)
                 }
             </div>
             {/*Tweet Button */}
-
             <div>
                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded-full w-56 h-12 shadow-md text-lg hidden xl:inline">
                     Tweet
@@ -42,15 +51,21 @@ const Sideber = () => {
 
             <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
                 <Image className="h-10 w-10 rounded-full xl:mr-2" width="50" height="50" src="/image/user.png" alt="user-img"></Image>
+
+                {/* The alt property is used to provide a fallback for browsers that do not support the srcset attribute. 
+                 // The width and height properties are used to set the dimensions of the image.
+                 // The rounded-full property is used to make the image rounded.
+                 // The xl:mr-2 property is used to move the image to the right on the large screen. */}
+
                 <div className="ml-2 leading-5 hidden xl:inline">
-                    <h4 className="font-bold">User Name</h4>
-                    <p className="text-sm text-gray-500">@userName</p>
+                    <h4 className="font-bold">User Name</h4> {/* User Name */}
+                    <p className="text-sm text-gray-500">@userName</p> {/* @userName */}
                 </div>
-                <DotsHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline" />
+                <DotsHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline" /> {/* DotsHorizontalIcon is a component that renders the SVG for the DotsHorizontal icon. */}
             </div>
 
         </div>
     );
 };
 
-export default Sideber;
+export default Sideber; 
